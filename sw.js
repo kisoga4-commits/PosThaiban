2c04d0aa18a242996c0a284e76f87103e9d4852e
kisoga4-commits <kisoga4@gmail.com>
2026-03-13 22:30:03 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,6 +2,8 @@ const CACHE_NAME = 'vpos-v11-cache';
 const assets = [
   './',
   './index.html',
+  './manifest.json',
+  './sw.js',
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/html5-qrcode',
   'https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;500;600;800;900&display=swap'
@@ -9,12 +11,16 @@ const assets = [
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
   );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
   );
 });
