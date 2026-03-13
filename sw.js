8cb6a7ec310e5234cbbdc663256d8122b0e5b4a1
kisoga4-commits <kisoga4@gmail.com>
2026-03-13 22:33:33 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vpos-v11-cache';
+const CACHE_NAME = 'vpos-v11-final-v1';
 const assets = [
   './',
   './index.html',
@@ -11,16 +11,12 @@ const assets = [
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
